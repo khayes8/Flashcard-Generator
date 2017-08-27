350b8bc8549d6440af4161c9c006e5bcd9430924
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-08-27 11:37:06 -0400

    prompts in command line now work so that user gets flashcard into when answering prompt

--- BasicCard.js
+++ BasicCard.js
@@ -2,15 +2,5 @@
 function BasicCard (front, back){
 	this.front = front;
 	this.back = back;
-	this.printInfo = function() {
-	    console.log(this.front + this.back);
-	  };
 }
-
-//call the function and implementing two arguments
-var firstPresident = new BasicCard ("Who was the first president of the United States? ", "George Washington");
-
-firstPresident.printInfo();
-console.log(firstPresident.front);
-console.log(firstPresident.back);
-
+module.exports = BasicCard;
--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var inquirer = require("inquirer");
 var basicCard = require("./BasicCard.js");
 var clozeCard = require("./ClozeCard.js");
 
-// inquirer.prompt([{
-//     name: 'song',
-//     message: 'Type in the name of a song.'
-// }]);
+var firstPresidentCloze = new clozeCard("George Washington", "George Washington was the first president of the United States.");
+
+
+var firstPresident = new basicCard("Who was the first president of the United States?", "George Washington");
 
 inquirer
   .prompt([
@@ -21,30 +21,25 @@ inquirer
       name: "clozeConfirm",
       default: true
     }
-     // if (answer.flashcard = "Cloze Card") {
-     // 	console.log(answer.flashcard);
-     // }
-
-     // else (answer.flashcard = "Basic Card") {
-     // 	console.log(answer.flashcard);
-     // }
 	])
    .then(function(answer) {
-    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
     if (answer.basicConfirm === true) {
-      console.log("You chose the basic flashcard");
+      console.log(firstPresident);
     }
 
     else if (answer.clozeConfirm === true) {
-      console.log("You chose the cloze flashcard");
+      console.log(firstPresidentCloze);
     }
-//If both equal true, tell the user they can only use on style of flashcards
-//If both equal false, tell the user "Okay, maybe you can study later."
+	});
+// If both equal true, tell the user they can only use on style of flashcards
+// If both equal false, tell the user "Okay, maybe you can study later."
 
 
+	 // if (answer.flashcard = "Cloze Card") {
+  //    	console.log(answer.flashcard);
+  //    }
 
+  //    else (answer.flashcard = "Basic Card") {
+  //    	console.log(answer.flashcard);
+  //    }
 
-    // else {
-    //   console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
-    // }
-  });
